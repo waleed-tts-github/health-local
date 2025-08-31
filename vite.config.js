5283c194fbfc03ccf0e3b50ca210251b1505bd83
waleed-tts-github <muhammadwaleed872.tts@gmail.com>
2025-08-31 10:32:34 -0700

    local deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-
-});
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		dedupe: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+	},
+})
