ae638bbabdd9eeaa848ac8fb204882d3dfbf3f9e
waleed-tts-github <muhammadwaleed872.tts@gmail.com>
2025-08-31 11:00:29 -0700

    firebase deploy done

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	resolve: {
-		dedupe: ['react', 'react-dom', 'react-router', 'react-router-dom'],
-	},
 })
